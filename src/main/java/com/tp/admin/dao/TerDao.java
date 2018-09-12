97ebcc3f355b8a8f2624623ad8b42f821ca1fff3
曾庆峰 <zqf@taipuauto.cn>
2018-09-12 11:08:30 +0800

    补充部分接口，修复部分bug

---
+++ src/main/java/com/tp/admin/dao/TerDao.java
@@ -0,0 +1,10 @@
+package com.tp.admin.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface TerDao {
+
+    List<Map<String, Object>> findAllTerIdAndTitle();
+
+}
--- src/main/java/com/tp/admin/service/OrderServiceI.java
+++ src/main/java/com/tp/admin/service/OrderServiceI.java
@@ -8,4 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 public interface OrderServiceI {
 
     ApiResult list(HttpServletRequest request , OrderSearch orderSearch);
+
+    ApiResult orderTerSelection(HttpServletRequest request);
 }
