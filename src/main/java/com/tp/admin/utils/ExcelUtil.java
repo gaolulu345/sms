5e2f22ad98675505ea26c3d108a0a2a313852f6e
曾庆峰 <zqf@taipuauto.cn>
2018-09-13 15:57:35 +0800

    代码进度更新

--- src/main/java/com/tp/admin/utils/ExcelUtil.java
+++ src/main/java/com/tp/admin/utils/ExcelUtil.java
@@ -14,13 +14,9 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -61,79 +57,5 @@ public class ExcelUtil {
                 .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
                 .body(new FileSystemResource(file));
     }
-
-
-//        json.put("orderID", "订单编号");
-//        json.put("terminalID", "洗车机编号");
-//        json.put("userID", "用户");
-//        json.put("status", "订单状态");
-//        json.put("sabisStr", "服务编号");
-//        json.put("payType", "支付方式");
-//        json.put("createTime", "创建时间");
-//        json.put("payTime", "支付时间");
-//        json.put("finishTime", "完成时间");
-//        json.put("amount", "总价");
-//        json.put("ticketID", "优惠券");
-//        json.put("cardID", "洗车卡");
-//        json.put("refundID", "退款编号");
-
-
-//        tmap.put(OrderState.OrderStateCreate.ordinal() + "", "创建");
-//        tmap.put(OrderState.OrderStateCancel.ordinal() + "", "撤销");
-//        tmap.put(OrderState.OrderStatePaid.ordinal() + "", "支付完成");
-//        tmap.put(OrderState.OrderStateWashing.ordinal() + "", "正在洗车");
-//        tmap.put(OrderState.OrderStateDone.ordinal() + "", "订单完成，洗车完毕");
-//        tmap.put(OrderState.OrderStateUnfinished.ordinal() + "", "洗车中断");
-//        tmap.put(OrderState.OrderStateDelayed.ordinal() + "", "延期洗车");
-    ////
-//        tmap.put(OrderType.AliPayOrder.ordinal() + "", "支付宝");
-//        tmap.put(OrderType.WxPayOrder.ordinal() + "", "微信");
-//        tmap.put(OrderType.TestOr0PayOrder.ordinal() + "", "测试或0元券");
-
-//        json.put("userID", "用户ID");
-//        json.put("type", "注册来源");
-//        json.put("phone", "手机号码");
-//        json.put("createTime", "注册时间");
-//        json.put("lastLoginTime", "上次登录时间");
-
-//        tmap.put(UserLoginType.LoginByPhoneMsg.ordinal() + "", "短信验证");
-//        tmap.put(UserLoginType.LoginByPhonePw.ordinal() + "", "手机号+密码");
-//        tmap.put(UserLoginType.LoginByWx.ordinal() + "", "APP内微信授权");
-//        tmap.put(UserLoginType.LoginByMiniWx.ordinal() + "", "微信小程序");
-//        tmap.put(UserLoginType.LoginByMiniAli.ordinal() + "", "支付宝小程序");
-//        json.put("refundID", "退款编号");
-//        json.put("reason", "退款原因");
-//        json.put("amount", "退款数额");
-//        json.put("orderID", "订单编号");
-//        json.put("createTime", "创建时间");
-//        json.put("modifyTime", "修改时间");
-//        json.put("checkTime", "审核时间");
-//        json.put("refundTime", "退款时间");
-//        json.put("sysAdminNameCheck", "审核人");
-//        json.put("sysAdminNamePay", "退款人");
-//        json.put("msg", "退款说明");
-//        json.put("status", "退款状态");
-//        tmap.put(RefundReasonType.MisPurchase.ordinal() + "", "误购，请求退款");
-//        tmap.put(RefundReasonType.TerDonnotStart.ordinal() + "", "设备无法启动");
-//        tmap.put(RefundReasonType.TerBreakOff.ordinal() + "", "洗车中通故障");
-//        tmap.put(RefundReasonType.UnSatified.ordinal() + "", "洗车服务不满意");
-//        tmap.put(RefundReasonType.OtherReason.ordinal() + "", "其它原因");
-//        tmap.put(RefundState.Default.ordinal() + "", "默认状态");
-//        tmap.put(RefundState.Asked.ordinal() + "", "请求退款");
-//        tmap.put(RefundState.Checked.ordinal() + "", "请求通过审核");
-//        tmap.put(RefundState.Paying.ordinal() + "", "已退款");
-//        tmap.put(RefundState.Denid.ordinal() + "", "请求被拒绝");
-//        tmpRow.createCell(0).setCellValue("序号");
-//        tmpRow.createCell(1).setCellValue("订单编号");
-//        tmpRow.createCell(2).setCellValue("网点名称");
-//        tmpRow.createCell(3).setCellValue("用户编号");
-//        tmpRow.createCell(4).setCellValue("支付时间");
-//        tmpRow.createCell(5).setCellValue("订单来源");
-//        tmpRow.createCell(6).setCellValue("支付方式");
-//        tmpRow.createCell(7).setCellValue("订单金额(分)");
-//        tmpRow.createCell(8).setCellValue("优惠券编号");
-//        tmpRow.createCell(9).setCellValue("洗车卡编号");
-//        tmpRow.createCell(10).setCellValue("外观清洗服务编号");
-//        tmpRow.createCell(11).setCellValue("内饰清洗服务编号");
 
 }
