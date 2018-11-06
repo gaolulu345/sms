25c522cdd201fb8f39b80fa25cc0404c74ea5e8d
曾庆峰 <zqf@taipuauto.cn>
2018-11-06 09:09:17 +0800

    代码进度更新

---
+++ src/main/java/com/tp/admin/controller/WxMiniMaintainAuthController.java
@@ -0,0 +1,11 @@
+package com.tp.admin.controller;
+
+/**
+ * 维保登录认证
+ */
+public class WxMiniMaintainAuthController {
+
+
+
+
+}
---
+++ src/main/java/com/tp/admin/controller/WxMiniMaintainManageController.java
@@ -0,0 +1,8 @@
+package com.tp.admin.controller;
+
+/**
+ * 维保管理
+ */
+public class WxMiniMaintainManageController {
+
+}
