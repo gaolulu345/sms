7335e344dcc3acaf07e9cc81ea40d9c1918a3266
tpsuzhao <suzhao@taipuauto.cn>
2018-11-09 10:36:17 +0800

    维保人员

--- src/main/resources/static/js/maintain_employee.js
+++ src/main/resources/static/js/maintain_employee.js
@@ -15,123 +15,52 @@ var vm = new Vue({
         currentPageIndex: null,
         deleted: false,
 
-        adminList: [],
-        rolesList: [],
-        adminSelection: [],
+        maintainList: [],
 
-        showEditAdmin: false,
-        showAddAdmin: false,
-
-        editAdminInfo: {},
-        addAdminInfo: {},
-
-        dialogLabelWidth: '200px',
+        // dialogLabelWidth: '200px',
     },
     
     mounted: function() {
     	console.log('mounted......')
-        this.getMaintainList(10, 1, false)
-        this.getRolesList()
+        this.getMaintainList(10, 1)
     },
 
     methods: {
-        getMaintainList: function(pageSize, pageIndex, deleted) {
-            this.$http.post("/api/private/admin/list", {
+        getMaintainList: function(pageSize, pageIndex) {
+            this.$http.post("/api/private/maintion/employee/list", {
                 pageSize: pageSize,
-                pageIndex: pageIndex,
-                deleted: deleted
+                pageIndex: pageIndex
             }).then(function(res){
                 let data = res.json().data;
                 let result = data.result;
                 result.forEach(function(val) {
-                    val.createTime = formatTimestampToSecond(val.createTime)
-                    val.modifyTime = formatTimestampToSecond(val.modifyTime)
                     val.lastLoginTime = formatTimestampToSecond(val.lastLoginTime)
                 })
-                vm.adminList = result;
+                vm.maintainList = result;
                 vm.totalCnt = data.totalCnt;
                 vm.currentPageSize = data.pageSize;
                 vm.currentPageIndex = data.pageIndex;
-                vm.deleted = data.deleted;
             })
         },
 
-        getRolesList: function() {
-            this.$http.post("/api/private/sys/list/roles", {
-                pageSize: 10000,
-                pageIndex: 1
-            }).then(function(res){
-                let data = res.json().data;
-                let result = data.result;
-                vm.rolesList = result;
-            })
-        },
 
-        // 注册
-        addAdmin: function() {
-            let regExp = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/; 
-            let infos = {
-                name: vm.addAdminInfo.name,
-                intros: vm.addAdminInfo.intros,
-                username: vm.addAdminInfo.username,
-                rolesId: vm.addAdminInfo.rolesId
-            }
-            if(!infos.username) {
-                vm.$message.error("请填写登录账户！")
-            } else if(!regExp.test(infos.username)) {
-                vm.$message.error("请填写正确的邮箱！")
-            } else {
-                this.$http.post("/api/private/admin/register", vm.addAdminInfo).then(function(res){
-                    let result = res.json();
-                    if(result.code == 200){
-                        vm.$message.success('已添加');
-                        vm.showAddAdmin = false;
-                        window.location.reload();
-                    }else {
-                        vm.$message.error(result.message)
-                    }
-                })
-            }
-            
-        },
-        // 修改信息
-        clickEditAdmin(row) {
-            console.log(row)
-            vm.editAdminInfo = row;
-            vm.showEditAdmin = true;
-        },
-        editAdmin: function() {
-            let data = {
-                id: vm.editAdminInfo.id,
-                intros: vm.editAdminInfo.intros,
-                name: vm.editAdminInfo.name,
-                rolesId: vm.editAdminInfo.rolesId
-            }
-            this.$http.post("/api/private/admin/update", data).then(function(res){
-                let result = res.json();
-                if(result.code == 200){
-                    vm.$message.success('已修改');
-                    vm.showEditAdmin = false;
-                    vm.getAdminList(vm.currentPageSize, vm.currentPageIndex, vm.deleted);
-                }else {
-                    vm.$message.error(result.message)
-                }
-            })
-        },
-        // 重置密码
-        resetPw: function(id) {
-            vm.$confirm('确认重置' + id + '号用户登录密码?', '提示', {
+
+        // 删除、禁用
+        clickDelete: function(id, deleted) {
+            let tip = deleted ? '禁用' : '解禁'
+            vm.$confirm('确认' + tip + '第' + id + '号维保人员?', '提示', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
                 type: 'warning'
             }).then(() => {
-                this.$http.post("/api/private/admin/reset/pw", {
-                    id: id
+                vm.$http.post("/api/private/maintion/employee/update/delete", {
+                    ids: [id],
+                    deleted: deleted
                 }).then(function(res){
                     let result = res.json();
                     if(result.code == 200){
-                        vm.$message.success('已重置');
-                        vm.getAdminList(vm.currentPageSize, vm.currentPageIndex, vm.deleted);
+                        vm.$message.success('已' + tip);
+                        vm.getMaintainList(vm.currentPageSize, vm.currentPageIndex);
                     }else {
                         vm.$message.error(result.message)
                     }
@@ -140,63 +69,41 @@ var vm = new Vue({
                 vm.$message.info('已取消操作')
             });
         },
-        // 批量删除
-        handleAdminSelectionChange(val) {
-            this.adminSelection = val;
-        },
-        updateDeleteAdmin: function(deleted) {
-            let tip = deleted ? '删除' : '恢复'
-            if (vm.adminSelection.length == 0) {
-                vm.$message.warning('请选择要' + tip + '的员工！');
-            } else {
-                let selection = vm.adminSelection
-                let ids = []
-                for(var key in selection) {
-                    ids.push(selection[key].id)
-                }
-                console.log(ids)
 
-                vm.$confirm('确认' + tip + '这' + ids.length + '个员工?', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    vm.$http.post("/api/private/admin/update/delete", {
-                        ids: ids,
-                        deleted: deleted
-                    }).then(function(res){
-                        let result = res.json();
-                        if(result.code == 200){
-                            vm.$message.success('已' + tip);
-                            vm.getAdminList(vm.currentPageSize, vm.currentPageIndex, vm.deleted);
-                        }else {
-                            vm.$message.error(result.message)
-                        }
-                    })
-                }).catch(() => {
-                    vm.$message.info('已取消操作')
-                });
-            }
+        // 审核
+        clickEnable: function(id, enable) {
+            let tip = enable ? '通过' : '拒绝'
+            vm.$confirm('确认' + tip + '第' + id + '号维保人员?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                vm.$http.post("/api/private/maintion/employee/update/enable", {
+                    ids: [id],
+                    enable: enable
+                }).then(function(res){
+                    let result = res.json();
+                    if(result.code == 200){
+                        vm.$message.success('已' + tip);
+                        vm.getMaintainList(vm.currentPageSize, vm.currentPageIndex);
+                    }else {
+                        vm.$message.error(result.message)
+                    }
+                })
+            }).catch(() => {
+                vm.$message.info('已取消操作')
+            });
         },
 
 
-        // 多选
-        toggleSelection(rows) {
-            if (rows) {
-                rows.forEach(row => {
-                    this.$refs.multipleTable.toggleRowSelection(row);
-                });
-            } else {
-                this.$refs.multipleTable.clearSelection();
-            }
-        },
+
 
 
         handleSizeChange(val) {
-            this.getAdminList(val, 1, vm.deleted)
+            this.getMaintainList(val, 1)
         },
         handleCurrentChange(val) {
-            this.getAdminList(vm.currentPageSize, val, vm.deleted)
+            this.getMaintainList(vm.currentPageSize, val)
         }
   
     }    
