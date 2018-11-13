7c59b986b99ed2dff3b08e0b8084a328336f4ef2
曾庆峰 <zqf@taipuauto.cn>
2018-11-13 17:03:03 +0800

    代码进度更新

--- src/main/java/com/tp/admin/controller/merchant/MerchantEmployeeController.java
+++ src/main/java/com/tp/admin/controller/merchant/MerchantEmployeeController.java
@@ -6,6 +6,7 @@ import com.tp.admin.data.search.MerchantEmployeeSearch;
 import com.tp.admin.service.MerchantEmployeeServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,17 +22,17 @@ public class MerchantEmployeeController {
     MerchantEmployeeServiceI merchantEmployeeService;
 
     @PostMapping(value = "/list")
-    public ApiResult list(HttpServletRequest request, MerchantEmployeeSearch merchantEmployeeSearch) {
+    public ApiResult list(HttpServletRequest request, @RequestBody  MerchantEmployeeSearch merchantEmployeeSearch) {
         return merchantEmployeeService.list(request, merchantEmployeeSearch);
     }
 
     @PostMapping(value = "/update/delete")
-    public ApiResult bachUpdateDeleted(HttpServletRequest request, MerchantEmployeeSearch merchantEmployeeSearch){
+    public ApiResult bachUpdateDeleted(HttpServletRequest request,@RequestBody  MerchantEmployeeSearch merchantEmployeeSearch){
         return merchantEmployeeService.bachUpdateDeleted(request, merchantEmployeeSearch);
     }
 
     @PostMapping(value = "/update/enable")
-    public ApiResult updateEnable(HttpServletRequest request, MerchantEmployeeSearch merchantEmployeeSearch){
+    public ApiResult updateEnable(HttpServletRequest request,@RequestBody  MerchantEmployeeSearch merchantEmployeeSearch){
         return merchantEmployeeService.updateEnable(request, merchantEmployeeSearch);
     }
 
