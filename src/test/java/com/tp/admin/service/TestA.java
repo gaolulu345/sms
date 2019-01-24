d6977bd6035f9913349b78473c6e151fe8f50174
宋娜 <sn@taipuauto.cn>
2019-01-24 11:15:53 +0800

    修改bug

--- src/test/java/com/tp/admin/service/TestA.java
+++ src/test/java/com/tp/admin/service/TestA.java
@@ -1,13 +1,6 @@
 package com.tp.admin.service;
 
-import java.sql.Timestamp;
-
 public class TestA extends Thread {
-
-    public static void main(String[] args) {
-        long time = new Timestamp(System.currentTimeMillis()).getTime();
-
-    }
 
 
 }
