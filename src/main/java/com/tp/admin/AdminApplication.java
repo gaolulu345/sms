7f2020ab50948771f9df6b505ba1bbd635593e4c
宋娜 <sn@taipuauto.cn>
2019-04-22 22:24:57 +0800

    修改

--- src/main/java/com/tp/admin/AdminApplication.java
+++ src/main/java/com/tp/admin/AdminApplication.java
@@ -50,21 +50,6 @@ public class AdminApplication {
 	}
 
 
-
-	/*@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);   config.addAllowedOrigin("http://localhost:9000");
-		config.addAllowedOrigin("null");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config); // CORS 配置对所有接口都有效
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
-		return bean;
-	}*/
-
 	/**
 	 * http重定向到https
 	 * @return
